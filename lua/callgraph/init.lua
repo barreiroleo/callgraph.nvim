cb332f029cc8300d3a967e936b677741e771bedf
Leo Barreiro <a.leo.barreiro@gmail.com>
2025-06-11 21:18:15 -0300

    ref: rework request/response handlers

--- lua/callgraph/init.lua
+++ lua/callgraph/init.lua
@@ -6,8 +6,13 @@ local M = {}
 function M.setup(config)
     vim.keymap.set("n", "<leader><leader>z", function()
         require("lazy.core.loader").reload("callgraph.nvim")
-        -- require("callgraph.lsp_adapter").test()
         vim.notify("Reloaded callgraph.nvim", vim.log.levels.WARN)
+    end, {})
+    vim.keymap.set("n", "<leader><leader>c", function()
+        require("lazy.core.loader").reload("callgraph.nvim")
+        Snacks.notifier.clear_history()
+        vim.notify("Reloaded callgraph.nvim", vim.log.levels.WARN)
+        vim.defer_fn(require("callgraph.lsp_adapter").test, 2)
     end, {})
 end
 
--- lua/callgraph/lsp_adapter.lua
+++ lua/callgraph/lsp_adapter.lua
@@ -1,11 +1,32 @@
----@class lsp.Handler
----@alias callgraph.Direction "in" | "out"
----@alias callgraph.HandlerWithDir fun(err: lsp.ResponseError?, result: any, context: lsp.HandlerContext, config?: table, dir: callgraph.Direction): ...any
+local Node = require("callgraph.tree.node")
 
 ---@class callgraph.Entry
 ---@field kind lsp.SymbolKind
 ---@field name string
 ---@field location lsp.URI
+
+---@class callgraph.Request.Ctx
+---@field root Node<callgraph.Entry>?
+---@field dir "in" | "out"
+
+---@class callgraph.Request
+---@field params lsp.TextDocumentPositionParams?
+---@field item lsp.CallHierarchyItem?
+---@field ctx callgraph.Request.Ctx
+
+---@class callgraph.Response.Lsp
+---@field err lsp.ResponseError?
+---@field result table
+---@field context lsp.HandlerContext
+---@field config table?
+
+---@class callgraph.Response
+---@field ctx callgraph.Request.Ctx
+---@field lsp callgraph.Response.Lsp
+
+---@alias callgraph.Handler fun(response: callgraph.Response.Lsp, ctx_callgraph: callgraph.Request.Ctx): ...any
+---@alias callgraph.Requester fun(client: vim.lsp.Client, ctx: callgraph.Request): integer?
+
 
 ---@return vim.lsp.Client?
 local function get_client()
@@ -20,39 +41,55 @@ local function get_client()
     return client
 end
 
-
----@type callgraph.HandlerWithDir
-local function handler_prepareCallHierarchy(err, result, ctx, config, dir)
-    ---@cast result lsp.CallHierarchyItem[]
-    if err or not result or vim.tbl_isempty(result) then
-        vim.notify("Could not prepare call hierarchy", vim.log.levels.ERROR)
-        return
+---@param response callgraph.Response.Lsp
+---@return vim.lsp.Client? client, table? result
+local function process_response(response)
+    if response.err or not response.result then
+        vim.notify("Bad repsonse", vim.log.levels.ERROR)
+        return nil, nil
     end
 
-    local client = vim.lsp.get_client_by_id(ctx.client_id)
+    local client = vim.lsp.get_client_by_id(response.context.client_id)
     if not client then
         vim.notify("Could not retrieve client from response", vim.log.levels.ERROR)
-        return
+        return nil, nil
     end
 
-    -- vim.print({ "result: ", result })
-    --
-    -- if dir == "in" then
-    --     request_incoming_calls(client, item, handler_incoming_calls)
-    -- else
-    --     request_outgoing_calls(client, item, handler_outgoing_calls)
-    -- end
+    if vim.tbl_isempty(response.result) then
+        vim.notify("Empty results. May hit a leaf call", vim.log.levels.TRACE)
+        return client, {}
+    end
+
+    return client, response.result
 end
 
 
----@param client vim.lsp.Client
----@param direction callgraph.Direction
----@return integer? request_id
-local function request_prepareCallHierarchy(client, direction)
-    local params = vim.lsp.util.make_position_params(0, client.offset_encoding)
-    local success, req_id = client:request('textDocument/prepareCallHierarchy', params, function(err, res, ctx, conf)
-        handler_prepareCallHierarchy(err, res, ctx, conf, direction)
-    end)
+---@type callgraph.Handler
+local function handler_prepareCallHierarchy(response, ctx)
+    local client, result = process_response(response)
+    if not client or not result then
+        vim.notify("Could not process call hierarchy response", vim.log.levels.ERROR)
+        return
+    end
+    ---@cast result lsp.CallHierarchyItem[]
+
+    --- Extract items from results and put into the tree
+    ctx.root.data = {
+        kind = result[1].kind, name = result[1].name, location = result[1].uri,
+    }
+
+    ---@type callgraph.Request
+    local request = { item = result[1], ctx = ctx }
+    -- M.request_incomingCalls(client, request)
+end
+
+---@type callgraph.Requester
+local function request_prepareCallHierarchy(client, request)
+    assert(request.params, "textDocument/prepareCallHierarchy requires TextDocumentPositionParams")
+    local success, req_id = client:request("textDocument/prepareCallHierarchy", request.params,
+        function(err, res, ctx, conf)
+            handler_prepareCallHierarchy({ err = err, result = res, context = ctx, config = conf }, request.ctx)
+        end)
     if not success then
         vim.notify("Failed to request call hierarchy", vim.log.levels.ERROR)
         return nil
@@ -61,17 +98,27 @@ local function request_prepareCallHierarchy(client, direction)
 end
 
 
-local M = {}
-function M.test()
-    local client = get_client()
 
-    if not client then
-        vim.notify("Failed to find recursive calls", vim.log.levels.ERROR)
-        return nil
+return {
+    test = function()
+        local client = get_client()
+        if not client then
+            vim.notify("Failed to find recursive calls", vim.log.levels.ERROR)
+            return nil
+        end
+
+        ---@type Node<callgraph.Entry>
+        local root = Node.new({})
+
+        ---@type callgraph.Request
+        local request = {
+            params = vim.lsp.util.make_position_params(0, client.offset_encoding),
+            ctx = {
+                dir = "in",
+                root = root,
+            }
+        }
+
+        request_prepareCallHierarchy(client, request)
     end
-
-    local direction = "in" --[[@as callgraph.Direction]]
-    request_prepareCallHierarchy(client, direction)
-end
-
-return M
+}
